cd752bac4483e6d35b684bc020bd0b9bf1bdee98
unknown <lganne2@yahoo.fr>
2015-05-08 22:24:15 +0200

    1er commit

---
+++ footer.php
@@ -0,0 +1,24 @@
+ï»¿<footer id="colophon" role="contentinfo">
+		<div id="supplementary" class="four">
+		
+		<!-- <aside class="widget">
+			<h5 class="widget-title">Liens</h5>-->
+				<!--<ul>
+				   <li><a href='http://www.ffst.fr/' class='tag-link-37' title='FFST' style='font-size: 10pt;'>Fédération Française de Shiatsu Traditionnel (FFST)</a></li>
+					    <li><a href='http://www.shiatsu-aist.org/' class='tag-link-33' title='AIST' style='font-size: 10pt;'>Association Internationale de Shiatsu Traditionnel (A.I.S.T.)</a></li>
+				 </ul>	-->
+					
+		<!--</aside>-->
+		 </div>
+	    </div>
+		<div id="site-generator">
+			 <strong>Heko Shiatsu</strong> - <strong>paris 13</strong>
+		</div>
+
+	</footer><!-- #colophon -->
+	</div><!-- #wrapper -->
+</div><!-- #page -->
+
+<script type="text/javascript" src="style/js/scripts.js"></script>
+</body>
+</html>
