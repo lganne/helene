4aff8b59309f343bdfdb6e73ac12eb997e032afb
unknown <alegoas@IY62.equad.fr>
2015-05-13 14:32:28 +0200

    initial

---
+++ bienfaits.php
@@ -0,0 +1,16 @@
+﻿<?php
+include_once('header.php');
+?>
+<div id="main">
+
+<div id="primary">
+  <div id="content" role="main">
+      
+      
+  
+  </div>
+ </div>
+ 
+ <?php
+include_once('footer.php');
+?>
