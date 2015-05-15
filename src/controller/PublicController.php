6ecd47c2a1e6f06eb7e4e070701cbb88386a8275
unknown <alegoas@IY62.equad.fr>
2015-05-15 13:12:44 +0200

    htaccess

---
+++ src/controller/PublicController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PublicController
+ *
+ * @author alegoas
+ */
+class PublicController {
+          //put your code here
+}
+
+?>
